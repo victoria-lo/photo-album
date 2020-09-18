3ec2abbf457e9d190b7b7a570f09f937d7785c7e
Victoria Lo <victoria2666@gmail.com>
2020-09-18 10:09:51 -0400

    add more storage functiond

--- src/Upload.js
+++ src/Upload.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import "./App.css";
 import app, { storage } from "./Firebase/config";
 import { AuthContext } from "./Firebase/context";
@@ -6,8 +6,12 @@ import { Redirect } from "react-router-dom";
 
 function Upload() {
   const [image, setImage] = useState(null);
+  const [allImages, setImages] = useState([]);
   const { user } = useContext(AuthContext);
 
+  useEffect(() => {
+    getFromFirebase();
+  }, []);
   const onImageChange = (e) => {
     const reader = new FileReader();
     let file = e.target.files[0];
@@ -30,12 +34,56 @@ function Upload() {
       const storageRef = storage.ref(user.email);
       const imageRef = storageRef.child(image.name);
       imageRef.put(image).then(() => {
-        alert("Image uploaded successfully to Firebase.");
+        storage
+          .ref(user.email)
+          .child(image.name)
+          .getDownloadURL()
+
+          .then((url) => {
+            if (allImages.indexOf(url) < 0) {
+              setImages((allImages) => [...allImages, url]);
+            }
+            console.log(allImages);
+            alert("Image uploaded successfully to Firebase.");
+          });
       });
     } else {
       alert("Please upload an image first.");
     }
   };
+
+  const getFromFirebase = () => {
+    let storageRef = storage.ref(user.email);
+    console.log(allImages);
+    storageRef
+      .listAll()
+      .then(function (res) {
+        res.items.forEach((imageRef) => {
+          imageRef.getDownloadURL().then((url) => {
+            if (allImages.indexOf(url) === -1) {
+              setImages((allImages) => [...allImages, url]);
+            }
+          });
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  const deleteFromFirebase = (url) => {
+    let pictureRef = storage.refFromURL(url);
+    pictureRef
+      .delete()
+      .then(() => {
+        setImages(allImages.filter((image) => image !== url));
+        alert("Picture is deleted successfully!");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div>
       {!user ? (
@@ -51,8 +99,21 @@ function Upload() {
             }}
           />
           <button onClick={() => uploadToFirebase()}>Upload to Firebase</button>
-          <div>
-            <button onClick={() => app.auth().signOut()}>Sign Out</button>
+          <button onClick={() => getFromFirebase()}>
+            Get Images from Firebase
+          </button>
+          <button onClick={() => app.auth().signOut()}>Sign Out</button>
+          <div id="photos">
+            {allImages.map((image) => {
+              return (
+                <div key={image} className="image">
+                  <img src={image} alt="" />
+                  <button onClick={() => deleteFromFirebase(image)}>
+                    Delete
+                  </button>
+                </div>
+              );
+            })}
           </div>
         </header>
       )}
