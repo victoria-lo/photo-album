c78ff46cf30d36c68aa30c30834b9d95b661631c
VicLo <victoria2666@gmail.com>
2020-09-30 12:13:20 -0400

    init tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
+import { shallow } from "enzyme";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it("renders without crashing", () => {
+  shallow(<App />);
+});
+
+it("renders Account header", () => {
+  const wrapper = shallow(<App />);
+  const welcome = (
+    <h1>
+      Welcome to Photo Album. This is a tutorial for Firebase Cloud Storage and
+      Authentication. Still in Progress.
+    </h1>
+  );
+  expect(wrapper.contains(welcome)).toEqual(false);
 });
